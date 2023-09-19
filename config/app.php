36060a7d069e8dfdef2320fc04ba3b15b4d02a52
DamienToscano <toscano.damien@gmail.com>
2023-09-19 13:08:29 +0200

    feat(repository): Register RepositoryServiceProvider in app config file

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FolioServiceProvider::class,
         App\Providers\VoltServiceProvider::class,
+        App\Providers\RepositoryServiceProvider::class,
     ])->toArray(),
 
     /*
