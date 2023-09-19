039b50709813280214439f28d8d605b794ccedf5
DamienToscano <toscano.damien@gmail.com>
2023-09-19 13:06:35 +0200

    feat(pokemon-data): Create DTO for Pokemon Data

---
+++ app/DataObjects/PokemonData.php
@@ -0,0 +1,24 @@
+<?php
+ 
+namespace App\DataObjects;
+ 
+readonly class PokemonData
+{
+    public function __construct(
+        public int $id,
+        public string $name,
+        public string $image,
+        public int $base_experience,
+        public int $height,
+        public int $weight,
+        public int $attack,
+        public int $defense,
+        public int $hp,
+        public int $speed,
+        public int $special_attack,
+        public int $special_defense,
+        public string $primary_type,
+        public ?string $secondary_type,
+    ) {
+    }
+}
